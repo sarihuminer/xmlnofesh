391085586cb5b5b29a921a9deeb589a3f3b6eaf1
sari huminer <sh6197174@gmail.com>
2020-02-13 00:37:55 +0200

    read from files

---
+++ readFromFiles/GoToFiles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readFromFiles
+{
+    class GoToFiles
+    {
+        private string v;
+
+        public GoToFiles()
+        {
+        }
+
+        public GoToFiles(string v)
+        {
+            this.v = v;
+        }
+    }
+}
---
+++ readFromFiles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readFromFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GoToFiles ob;
+            if (args.Length == 0)
+                ob = new GoToFiles();
+            else
+                ob = new GoToFiles(args[0]);
+
+
+            ob.Save();
+
+            List<string> olds = ob.GetFromFile();
+            foreach (var item in olds)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
